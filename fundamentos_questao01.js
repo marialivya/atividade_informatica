fbe87fb73fb752bb1cec8b6c1c0c82cdf24e1b3f
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:10:34 -0300

    Update fundamentos_questao01.js

--- fundamentos_questao01.js
+++ fundamentos_questao01.js
@@ -1,6 +1,8 @@
 const hoje = new Date();
 const diasDaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+
+const diaSemana = diasDaSemana[hoje.getDay()];
 const horaAtual = hoje.toLocaleTimeString('pt-BR');
 
-console.log(`Hoje é: ${diasDaSemana[hoje.getDay()]}.`);
+console.log(`Hoje é: ${diaSemana}.`);
 console.log(`A hora atual é: ${horaAtual}.`);
