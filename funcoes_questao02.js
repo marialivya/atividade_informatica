6d75f4b63b82f71f25415e72637ca251119f6d17
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:29:01 -0300

    Update funcoes_questao02.js

--- funcoes_questao02.js
+++ funcoes_questao02.js
@@ -1,1 +1,17 @@
+function contarVogaisConsoantes(frase) {
+    const vogais = 'aeiouAEIOU';
+    let vogaisCount = 0;
+    let consoantesCount = 0;
 
+    for (let char of frase) {
+        if (vogais.includes(char)) {
+            vogaisCount++;
+        } else if (char.match(/[a-z]/i)) {
+            consoantesCount++;
+        }
+    }
+
+    return { vogais: vogaisCount, consoantes: consoantesCount };
+}
+
+console.log(contarVogaisConsoantes("Hello World"));
