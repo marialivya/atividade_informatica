b7582ed2b1ee2ba748e152a7383323eb2091362f
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:20:46 -0300

    Update fundamentos_questao06.js

--- fundamentos_questao06.js
+++ fundamentos_questao06.js
@@ -1,1 +1,8 @@
+const numeros = [1, 2, 3, 2, 2, 4, 5, 6, 2];
 
+const ocorrencias = {};
+numeros.forEach(num => ocorrencias[num] = (ocorrencias[num] || 0) + 1);
+
+const maisFrequente = Object.keys(ocorrencias).reduce((a, b) => ocorrencias[a] > ocorrencias[b] ? a : b);
+
+console.log(`O número com mais ocorrências é: ${maisFrequente}`);
