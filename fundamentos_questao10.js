8cbd8f976acdc3737260f20415f6a7d8db7dd1ab
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:36:39 -0300

    Update fundamentos_questao10.js

--- fundamentos_questao10.js
+++ fundamentos_questao10.js
@@ -1,1 +1,5 @@
+const nomeCompleto = "Alan Mathison Turing";
+const [primeiroNome, ...resto] = nomeCompleto.split(' ').reverse();
+const iniciais = resto.map(nome => nome[0] + '.').join(' ');
 
+console.log(`${iniciais} ${primeiroNome}`);
