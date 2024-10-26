be34e955c498f97c1ba2701bdb227a656229333b
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:30:00 -0300

    Update funcoes_questao05.js

--- funcoes_questao05.js
+++ funcoes_questao05.js
@@ -1,1 +1,14 @@
+function maiorPalavra(frase) {
+    const palavras = frase.split(' ');
+    let maior = '';
 
+    for (let palavra of palavras) {
+        if (palavra.length > maior.length) {
+            maior = palavra;
+        }
+    }
+
+    return maior;
+}
+
+console.log(maiorPalavra("A inteligência artificial é fascinante"));
