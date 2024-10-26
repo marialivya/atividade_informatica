1ee88905a0f7fca2e31679b71041f687d042eb4e
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:28:27 -0300

    Update condicionais_questao04.js

--- condicionais_questao04.js
+++ condicionais_questao04.js
@@ -1,1 +1,12 @@
+const alunos = [
+    ["Aluno1", [10, 50, 100]],
+    ["Aluno2", [55, 78, 90]],
+    ["Aluno3", [30, 60, 80]]
+];
 
+alunos.forEach(aluno => {
+    const [nome, notas] = aluno;
+    const media = notas.reduce((acc, nota) => acc + nota, 0) / notas.length;
+    const status = media >= 70 ? 'Aprovado' : 'Reprovado';
+    console.log(`${nome}, média ${media.toFixed(2)}, ${status}`);
+});
