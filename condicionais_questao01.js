097f7aa626e3041b9045d7542b8c2dc72c405f25
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:27:36 -0300

    Update condicionais_questao01.js

--- condicionais_questao01.js
+++ condicionais_questao01.js
@@ -1,1 +1,10 @@
+const num1 = 10;
+const num2 = 20;
 
+if (num1 > num2) {
+    console.log(`${num1} é maior`);
+} else if (num2 > num1) {
+    console.log(`${num2} é maior`);
+} else {
+    console.log("Os números são iguais");
+}
