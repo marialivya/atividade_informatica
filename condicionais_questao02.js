d7395e3600522af43b06bd09ba99d036ad0dc71c
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:27:52 -0300

    Update condicionais_questao02.js

--- condicionais_questao02.js
+++ condicionais_questao02.js
@@ -1,1 +1,6 @@
-
+for (let i = 0; i <= 50; i++) {
+    console.log(`${i} é ${i % 2 === 0 ? 'par' : 'ímpar'}`);
+}
+for (let i = 0; i <= 50; i++) {
+    console.log(`${i} é ${i % 2 === 0 ? 'par' : 'ímpar'}`);
+}
