798eaa7e65703bde601f4939b65891af652d943a
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-26 16:15:17 -0300

    Update fundamentos_questao03.js

--- fundamentos_questao03.js
+++ fundamentos_questao03.js
@@ -1,1 +1,13 @@
+function celsiusParaFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
 
+function fahrenheitParaCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+const celsius = 30;
+const fahrenheit = 86;
+
+console.log(`${celsius}°C é igual a ${celsiusParaFahrenheit(celsius)}°F`);
+console.log(`${fahrenheit}°F é igual a ${fahrenheitParaCelsius(fahrenheit).toFixed(2)}°C`);
