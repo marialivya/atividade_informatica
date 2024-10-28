f7efac8847c98efba1cae8691a7f13382a0f491d
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-28 16:21:11 -0300

    Update fundamentos_questao05.js

--- fundamentos_questao05.js
+++ fundamentos_questao05.js
@@ -1,8 +1,8 @@
-const a = 60;
-const b = 75;
-const c = 90;
+const num1 = 60;
+const num2 = 75;
+const num3 = 90;
 
-if ((a >= 50 && a <= 99) && (b >= 50 && b <= 99) && (c >= 50 && c <= 99)) {
+if ((num1 >= 50 && num1 <= 99) && (num2 >= 50 && num2 <= 99) && (num3 >= 50 && num3 <= 99)) {
     console.log("Sucesso");
 } else {
     console.log("Erro");
