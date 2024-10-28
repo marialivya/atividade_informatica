b9a7e9481d6074b2a2f11cc68443e326a06c1395
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-28 16:11:57 -0300

    Update fundamentos_questao02.js

--- fundamentos_questao02.js
+++ fundamentos_questao02.js
@@ -1,7 +1,6 @@
-const a = 3;
-const b = 4;
-const c = 5;
-const s = (a + b + c) / 2;
-const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+const altura = 3;
+const base = 4;
+
+const area = (base * altura) / 2;
 
 console.log(`A área do triângulo é: ${area.toFixed(2)}`);
