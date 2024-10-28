425cbe593a96d8ec37143f64699e68625453375a
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-28 16:40:11 -0300

    Update fundamentos_questao09.js

--- fundamentos_questao09.js
+++ fundamentos_questao09.js
@@ -1,1 +1,4 @@
+const frase = "Javascript é muito interessante";
+const somaTamanhos = frase.split(' ').reduce((acc, palavra) => acc + palavra.length, 0);
 
+console.log(`A soma dos tamanhos das palavras é: ${somaTamanhos}`);
