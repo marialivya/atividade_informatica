be793ec859dee936df12e3b0017d2c300a4d52e2
marialivya <108295828+marialivya@users.noreply.github.com>
2024-10-28 16:36:38 -0300

    Update fundamentos_questao07.js

--- fundamentos_questao07.js
+++ fundamentos_questao07.js
@@ -1,1 +1,6 @@
+const frase = "Deus é perfeito e maravilhoso";
+const palavrasModificadas = frase.split(' ').map(palavra => {
+    return palavra.length < 5 ? palavra.toUpperCase() : palavra.toLowerCase();
+}).join(' ');
 
+console.log(palavrasModificadas);
